0e45b52703c1c1bc2842d9d8b2f84f785c97939f
Ryan Mast <mast9@llnl.gov>
2018-05-03 13:10:38 -0700

    Add a Travis config for building documentation

---
+++ scripts/update-documentation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Install required python packages
+pip3 install ghp-import
+pip3 install breathe
+pip3 install sphinx_rtd_theme
+pip3 install nbsphinx
+pip3 install sphixcontrib-pandoc-markdown
+
+# also has github target
+make doxygen html
