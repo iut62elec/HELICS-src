3c52384c54b4b809a395b59bf7be9fe3a98010f6
Ryan Mast <mast9@llnl.gov>
2018-05-03 16:10:07 -0700

    Add ipython (hopefully last to get working)

--- scripts/update-documentation.sh
+++ scripts/update-documentation.sh
@@ -11,6 +11,8 @@ python3 -m pip install breathe
 python3 -m pip install sphinx_rtd_theme
 python3 -m pip install nbsphinx
 python3 -m pip install sphinxcontrib-pandoc-markdown
+python3 -m pip install ipython
 
 # also has github target
 make doxygen html
+
