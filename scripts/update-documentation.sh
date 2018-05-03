6ab4b52ea193f9a62016fc99c09a7c7b0155732e
Ryan Mast <mast9@llnl.gov>
2018-05-03 14:03:56 -0700

    Fix typo in package name

--- scripts/update-documentation.sh
+++ scripts/update-documentation.sh
@@ -5,7 +5,7 @@ python3 -m pip install --user ghp-import
 python3 -m pip install --user breathe
 python3 -m pip install --user sphinx_rtd_theme
 python3 -m pip install --user nbsphinx
-python3 -m pip install --user sphixcontrib-pandoc-markdown
+python3 -m pip install --user sphinxcontrib-pandoc-markdown
 
 # also has github target
 make doxygen html
