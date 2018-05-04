4bad6575d6ef5dba857c7c2e199f1c58ce50d857
Ryan Mast <mast9@llnl.gov>
2018-05-04 12:42:36 -0700

    Use patched version of sphinxcontrib-pandoc-markdown

--- scripts/update-documentation.sh
+++ scripts/update-documentation.sh
@@ -5,12 +5,12 @@ pip --version
 pip3 --version
 
 # Install required python packages
-python3 -m pip install sphinx==1.6.7
+python3 -m pip install sphinx
 python3 -m pip install ghp-import
 python3 -m pip install breathe
 python3 -m pip install sphinx_rtd_theme
 python3 -m pip install nbsphinx
-python3 -m pip install sphinxcontrib-pandoc-markdown
+python3 -m pip install git+https://github.com/kdheepak/sphinxcontrib-pandoc-markdown.git@fix-StringList-AttributeError
 python3 -m pip install ipython
 
 # also has github target
