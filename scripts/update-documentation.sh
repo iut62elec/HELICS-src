4419cf5b92bd2dbd40833e29ec7c0f973db8a7e4
Ryan Mast <mast9@llnl.gov>
2018-05-04 13:22:12 -0700

    Install pip modules as user

--- scripts/update-documentation.sh
+++ scripts/update-documentation.sh
@@ -5,13 +5,13 @@ pip --version
 pip3 --version
 
 # Install required python packages
-python3 -m pip install sphinx
-python3 -m pip install ghp-import
-python3 -m pip install breathe
-python3 -m pip install sphinx_rtd_theme
-python3 -m pip install nbsphinx
-python3 -m pip install git+https://github.com/kdheepak/sphinxcontrib-pandoc-markdown.git@fix-StringList-AttributeError
-python3 -m pip install ipython
+pip install -U sphinx
+pip install -U ghp-import
+pip install -U breathe
+pip install -U sphinx_rtd_theme
+pip install -U nbsphinx
+pip install -U git+https://github.com/kdheepak/sphinxcontrib-pandoc-markdown.git@fix-StringList-AttributeError
+pip install -U ipython
 
 # also has github target
 make doxygen html
